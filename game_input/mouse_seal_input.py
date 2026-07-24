493ab8219450cc325a479f9e292cb40df09c493d
JJuNi27 <102855109+JJuNi27@users.noreply.github.com>
2026-07-24 14:17:43 +0900

    refactor: allow Gojo seal input reset

--- game_input/mouse_seal_input.py
+++ game_input/mouse_seal_input.py
@@ -6,13 +6,12 @@ from game.domain_controller import DomainController
 
 
 class MouseSealInput:
-    """마우스 장인 입력을 DomainController의 의미 단위 명령으로 변환한다.
-
-    실제 버튼 이벤트는 이 클래스만 알고 있다. 나중에 카메라 장인 인식을
-    추가할 때는 같은 controller 메서드를 호출하는 CameraSealInput을 만들면 된다.
-    """
+    """마우스 장인 입력을 고죠 Controller의 의미 단위 명령으로 변환한다."""
 
     def __init__(self) -> None:
+        self._right_held = False
+
+    def reset(self) -> None:
         self._right_held = False
 
     def handle_event(
