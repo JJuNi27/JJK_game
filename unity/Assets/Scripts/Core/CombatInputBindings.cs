78f128e74311ae230ad136c4114df7a999dd92a4
JJuNi27 <102855109+JJuNi27@users.noreply.github.com>
2026-07-26 22:43:02 +0900

    Centralize combat input bindings

---
+++ unity/Assets/Scripts/Core/CombatInputBindings.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace JJKGame.Core
+{
+    public static class CombatInputBindings
+    {
+        public const KeyCode Skill1 = KeyCode.Q;
+        public const KeyCode Skill2 = KeyCode.E;
+        public const KeyCode Ultimate = KeyCode.R;
+        public const KeyCode Domain = KeyCode.V;
+        public const KeyCode CancelCommand = KeyCode.X;
+        public const KeyCode Dodge = KeyCode.Space;
+
+        public const string Skill1Label = "Q";
+        public const string Skill2Label = "E";
+        public const string UltimateLabel = "R";
+        public const string DomainLabel = "V";
+        public const string CancelCommandLabel = "X";
+        public const string DodgeLabel = "SPACE";
+    }
+}
